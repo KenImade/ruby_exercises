704fcf303c541178daf4243f5c63766837d77553
Kenneth Imade <kenneth.imade@yahoo.com>
2021-11-26 22:19:42 +0100

    Bubble Sort Project Added

---
+++ bubble_sort/bubble_sort.rb
@@ -0,0 +1,21 @@
+def bubble_sort array
+    swapped = true
+    checker = (array.length-1)
+    while swapped
+        for index in 0...(array.length-1)
+            if array[index] > array[index+1]
+                swap = array[index]
+                array[index] = array[index+1]
+                array[index+1] = swap
+            end
+        end
+
+        checker = checker - 1
+        if checker == 0
+            swapped = false
+        end
+    end
+    array
+end
+
+bubble_sort([4,3,78,2,0,2])
